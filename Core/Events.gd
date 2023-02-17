d9e8c98ee63829005cb80a1e15fb1956c0c3df4b
ThiDiamondDev <thi.dimond1@gmail.com>
2023-02-17 15:29:36 -0300

    Add auto load Events script.

---
+++ Core/Events.gd
@@ -0,0 +1,26 @@
+extends Node
+
+signal collected(item)
+signal interacted(args)
+signal dialogued(args)
+signal can_interact(args)
+signal found_file(args)
+signal dialog_finished()
+signal update_objective(args)
+signal file_closed(args)
+
+# Remove signal never emitted warning
+func emit_signals():
+    if false:
+        emit_signal("collected", "")
+        emit_signal("interacted", "")
+        emit_signal("dialogued", "")
+        emit_signal("can_interact", "")
+        emit_signal("found_file", "")
+        emit_signal("dialog_finished","")
+        emit_signal("update_objective","")
+        emit_signal("file_closed","")
+
+
+var file_found := {}
+var potions_found := 0
