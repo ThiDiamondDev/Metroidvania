de5a197151d8a194437e49c9ac33963ad6a9bd7a
ThiDiamondDev <thi.dimond1@gmail.com>
2023-02-17 15:28:13 -0300

    Add dialog scene

---
+++ Core/Dialog.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var dialog_list := ["oiii", "Hello i am a test", "Helllo worlddddd"]
+
+onready var animation_plyer: AnimationPlayer = $AnimationPlayer
+onready var label : RichTextLabel = $Label
+
+var dialog_name: String
+
+func _ready():
+	label.percent_visible = 0
+	var __  
+	__ = animation_plyer.connect("animation_finished", self, "on_animation_finished")
+	__ = Events.connect("dialogued", self, "play_dialog")
+	
+func set_next_text():
+	if  dialog_list.size() > 0:
+		label.bbcode_text =  get_current_text()
+		animation_plyer.play("dialog")
+	else:
+		Events.emit_signal("dialog_finished")
+	
+func get_current_text():
+	return "\n[center]" + dialog_list[0] + "[/center]"
+
+func on_animation_finished(_animation_name):
+	label.percent_visible = 0
+	dialog_list.pop_front() 	
+	set_next_text()
+
+func play_dialog(dialog: Array):
+	dialog_list = dialog
+	set_next_text()
